4f7281ab42e628138ef753a2efb5a19079b2f9f8
Nitin Singh <nitinsingh.iitp@gmail.com>
2024-12-20 22:13:48 +0530

    .

--- frontend/src/landing/home/HomePage.js
+++ frontend/src/landing/home/HomePage.js
@@ -1,4 +1,6 @@
 import React from "react";
 import Hero from "./Hero";
 import Awards from "./Awards";
-import
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Education from "./Education";
