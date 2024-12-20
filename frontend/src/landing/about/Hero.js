cacbe093c5f77e6c1356d3c10e030203f82c236f
Nitin Singh <nitinsingh.iitp@gmail.com>
2024-12-20 21:51:55 +0530

    upadte

---
+++ frontend/src/landing/about/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2 text-center">
+          We pioneered the discount broking model in India
+          <br />
+          Now, we are breaking ground with our technology.
+        </h1>
+      </div>
