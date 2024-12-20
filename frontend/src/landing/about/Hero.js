149dc3591792554234fd6b5041eeea8330ecaf8c
Nitin Singh <nitinsingh.iitp@gmail.com>
2024-12-20 21:53:10 +0530

    .

--- frontend/src/landing/about/Hero.js
+++ frontend/src/landing/about/Hero.js
@@ -16,3 +16,14 @@ function Hero() {
         style={{ lineHeight: "1.8", fontSize: "1.2em" }}
       >
         <div className="col-6 p-5">
+        <p>
+            We kick-started operations on the 15th of August, 2010 with the goal
+            of breaking all barriers that traders and investors face in India in
+            terms of cost, support, and technology. We named the company
+            Zerodha, a combination of Zero and "Rodha", the Sanskrit word for
+            barrier.
+          </p>
+          <p>
+            Today, our disruptive pricing models and in-house technology have
+            made us the biggest stock broker in India.
+          </p>
